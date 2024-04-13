a021b2ec9c5ec18abac0196643afd1adb39cfb4b
Diya Mahajan <mahajandiya762@gmail.com>
2024-04-14 03:51:48 +0530

    Intropage

---
+++ frontend/src/Pages/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div>
+      <div class="h-[680px] bg-gradient-to-r from-cyan-600 to-slate-800"></div>
+    </div>
+  )
+}
+
+export default Intro
