9ebf1c57a917be8097a4d5c17f05b1f23e046bac
Diya Mahajan <mahajandiya762@gmail.com>
2024-04-29 02:49:30 +0530

    height changes

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Intro from './Intro'
 function Home() {
   return (
-    <div className='h-[800px] overflow-hidden bg-gradient-to-r from-[#b8cdff] to-[#003e53]'>
+    <div className='h-[750px] overflow-hidden bg-gradient-to-r from-[#b8cdff] to-[#003e53]'>
     
         <Intro/></div>
   )
