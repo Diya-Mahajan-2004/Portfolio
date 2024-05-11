03cd1e4ec2d98858e2fad2bfde9cc9d5a4b1258a
Diya Mahajan <mahajandiya762@gmail.com>
2024-05-11 22:42:45 +0530

    text transition

--- frontend/src/Pages/Intro.jsx
+++ frontend/src/Pages/Intro.jsx
@@ -1,33 +1,33 @@
-import React from "react";
+import { useEffect } from "react";
+
+import React, { useState } from "react";
+
 import "../CSS/Intro.css";
 
 function Intro() {
+  const texts = [
+    "Hi, I'm Diya Mahajan",
+    "A Full-Stack Web Developer",
+    "& a Web Designer",
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const nextIndex = (currentIndex + 1) % texts.length;
+      setCurrentIndex(nextIndex);
+    }, 4000);
+
+    return () => clearInterval(intervalId); // Cleanup function to stop interval on unmount
+  }, [currentIndex, texts]);
+
   return (
     <div>
-      <div className=" justify-center align-middle text-center md:mt-52 font-semibold">
-        <h1 className=" text-white md:text-[3rem]">
-          Hi, I'm <span className="text-[#734697]">Diya Mahajan</span>{" "}
-        </h1>
-        <h1 className="md:text-[2.2rem] md:ml-3 text-white">
-          A Full-Stack Developer <br /> & UI/UX Designer.
-        </h1>
-        <button className="hover:text-[#8121D0] hover:bg-white bold  hover:border-white bg-[#291C3A]  text-white font-semibold border-4 py-3 px-8 rounded-full mt-6 ">
-          Contact Me For Work
-        </button>
+      <div className=" justify-center align-middle text-center md:mt-72 font-semibold">
+        <h1 className=" text-white md:text-[3rem]">{texts[currentIndex]}</h1>
       </div>
-      <div></div>
     </div>
   );
 }
 
 export default Intro;
-
-/*
-<div className='  text-2xl md:ml-28 '>
-       
-        <h1 className=' md:text-[3rem] md:mt-52'>Hi, I'm Diya </h1>
-        <h1 className='md:text-[2rem] mt-8'>A Web Developer.</h1>
-
-
-    </div>
-*/
