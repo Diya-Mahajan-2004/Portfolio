8bd43742da06fe640da7dcb07d442b61bf02490e
Diya Mahajan <mahajandiya762@gmail.com>
2024-05-23 02:20:59 +0530

    //

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Intro from "./Intro";
 import Navbar from "../Components/Navbar";
 import About from "./About";
+import Skills from "./Skills"
 function Home() {
   return (
     <div>
@@ -9,6 +10,7 @@ function Home() {
       <div className=" overflow-hidden bg-gradient-to-r from-[#272f42] to-[#212a40]  ">
         <Intro />
         <About/>
+        <Skills/>
       </div>
     </div>
   );
